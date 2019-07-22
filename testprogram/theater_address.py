f38939c7e7529e80af50e7e3b51ce9b22251d067
Simon Su <simon.mailtest.0910@gmail.com>
2019-07-22 09:23:55 +0800

    'toheroku0722'

--- testprogram/theater_address.py
+++ testprogram/theater_address.py
@@ -1,6 +1,6 @@
-# import requests
-# from bs4 import BeautifulSoup
-# import re
+import requests
+from bs4 import BeautifulSoup
+import re
 
 # url = "https://www.google.com/search?q={}".format('國賓影城(台北長春廣場)')
 # # url = 'https://movies.yahoo.com.tw/ajax/pc/get_schedule_by_movie?movie_id=9467&date=2019-07-17'
@@ -9,27 +9,31 @@
 # address = ((theater_search.select('div.AVsepf span.BNeawe.tAd8D.AP7Wnd'))[0]).text
 # print(address)
 
-# 'AreaList-1952slimofy.tkBeautifoulikeslimofy.tk123'
+'AreaList-1952slimofy.tkBeautifoulikeslimofy.tk123'
+
+url ='https://861e7e38.ngrok.io/line_bot/iwantdeleteallobject'
+response = requests.get(url)
+print(response.text)
 
 
 
 
-c = 'AreaList-1952slimofy.tkBeautifoull-1982slimofy.tk'
-area = 'AreaList-'
-beauti = 'Beautifoull-'
+# c = 'AreaList-1952slimofy.tkBeautifoull-1982slimofy.tk'
+# area = 'AreaList-'
+# beauti = 'Beautifoull-'
 
-i = c.split('slimofy.tk')
-# b = x if 'AreaList-' else 'Beautifoul' for x in i
+# i = c.split('slimofy.tk')
+# # b = x if 'AreaList-' else 'Beautifoul' for x in i
 
-b = [ x if beauti in x else None for x in i ]
+# b = [ x if beauti in x else None for x in i ]
 
-print(b)
-# max = lambda m, n: m if m > n else n
-# max = lambda b:b[i] if  area in b[i] for i in range(len(b))
-def zz(b):
-    for i in b:
-        if area in i: 
-            return i 
+# print(b)
+# # max = lambda m, n: m if m > n else n
+# # max = lambda b:b[i] if  area in b[i] for i in range(len(b))
+# def zz(b):
+#     for i in b:
+#         if area in i: 
+#             return i 
 # print(zz(b))
 
 
