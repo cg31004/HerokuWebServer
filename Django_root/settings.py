3d46ee2e6b2d8c2cadc7da004514d30b86d001b2
Simon Su <simon.mailtest.0910@gmail.com>
2019-07-30 16:39:48 +0800

    17

--- Django_root/settings.py
+++ Django_root/settings.py
@@ -173,8 +173,8 @@ LINE_CHANNEL_ACCESS_TOKEN = LINE_CHANNEL_ACCESS_TOKEN
 LINE_CHANNEL_SECRET = LINE_CHANNEL_SECRET
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-if 'dyno' in os.environ:
-    DEBUG = True
-    # import dj_database_url
-    # db_from_env = dj_database_url.config()
-    # DATABASES['default'].update(db_from_env)
+# if 'dyno' in os.environ:
+#     DEBUG = True
+#     import dj_database_url
+#     db_from_env = dj_database_url.config()
+#     DATABASES['default'].update(db_from_env)
